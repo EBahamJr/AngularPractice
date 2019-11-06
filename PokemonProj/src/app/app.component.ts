371203d3036716b5d2e495c58cb03775a8434750
EBahamJr <ejbaham01@gmail.com>
2019-11-06 16:10:17 -0500

    Fixed naming

--- PokemonProj/src/app/app.component.ts
+++ PokemonProj/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+//poke-api service
+import { PokeAPIserveService } from './poke-apiserve.service';
+//pokemon component
+import {Pokemon} from './Pokemon';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PokemonProj';
+  //empty list for pokemon's data
+  pokemon: Pokemon[] = [];
+  //check if loading
+  isLoading: boolean = false;
+  error: boolean = false;
+
+  constructor(private pokedexService: PokeAPIserveService){}
+  //activate on initialization (whenever this component is created, it will load 9 more pokemon.)
+  ngOnInit(){
+    this.loadMore();
+  }
+  //Load initial data
+  loadMore() {
+    this.isLoading = true;
+    //use service to load the next 9 mons
+    this.pokedexService.getPokemon(this.pokemon.length, 9)
+    //loading the next 9 pokemon, then putting them in the list.
+      .then(pokemon => {
+        pokemon = pokemon.map(p => {
+          //defaults to false, to avoid loading on screen immediately(looks weird otherwise)
+          p.imageLoaded = false;
+          return p;
+        });
+        //if successful, append result to list
+        this.pokemon = this.pokemon.concat(pokemon);
+        this.isLoading = false;
+        this.error = false;
+      })
+    this.error = true;
+    this.isLoading = false;
+  }
 }
